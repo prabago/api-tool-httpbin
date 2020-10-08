bcf6883bcddcf94aa78799a6a51d5941d5d359c9
Pablo Rábago <prabagodelrio@gmail.com>
2020-10-08 11:02:20 +0200

    SB_TOKEN_URL

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,10 +6,6 @@ apictl version
 rm -rf $HOME/.wso2apictl
 
 echo 'setting up dev environment'
-apictl add-env -e local-lv \
+apictl add-env -e local-live \
                     --apim https://localhost:9443 \
                     --token https://localhost:8243/token
-
-apictl add-env -e local-sb \
-                    --apim https://localhost:9443 \
-                    --token https://localhost:8244/token
