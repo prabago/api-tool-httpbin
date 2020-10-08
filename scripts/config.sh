97cb4ac0026b14c445f551421930e5fd8c93cf5a
Pablo Rábago <prabagodelrio@gmail.com>
2020-10-08 11:15:22 +0200

    prueba

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,6 +6,10 @@ apictl version
 rm -rf $HOME/.wso2apictl
 
 echo 'setting up dev environment'
-apictl add-env -e local \
+apictl add-env -e local-lv \
                     --apim https://localhost:9443 \
                     --token https://localhost:8243/token
+
+apictl add-env -e local-sb \
+                    --apim https://localhost:9443 \
+                    --token https://localhost:8244/token
